8504d63b793b2a1132e52a5e76a827568a36933e
Kopylov Sergey <kopylov.serge@gmail.com>
2022-02-02 22:08:12 +0300

    ДЗ1. Задача №1 (Решение квадратного уравнения).

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discriminant;
+  discriminant = b ** 2 - 4 * a * c;
+  let x1, x2;
+
+  if (discriminant === 0) {
+    x1 = -b / (2 * a);
+  } else if (discriminant > 0) {
+    x1 = (-b + Math.sqrt(discriminant) ) / (2 * a);
+    x2 = (-b - Math.sqrt(discriminant) ) / (2 * a);
+  }
+
+  if (x1 !== undefined) { arr.push(x1); }
+  if (x2 !== undefined) { arr.push(x2); }
+
+  console.log(discriminant);
   return arr; // array
 }
 
