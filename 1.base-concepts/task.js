3ece27044714d0fd4d88a48fa656eb7fcc06ea5e
Kopylov Sergey <kopylov.serge@gmail.com>
2022-02-03 13:56:17 +0300

    ДЗ1. Задача №2, но с ошибками в алгоритме расчета.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -16,14 +16,51 @@ function solveEquation(a, b, c) {
   if (x1 !== undefined) { arr.push(x1); }
   if (x2 !== undefined) { arr.push(x2); }
 
-  console.log(discriminant);
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let loanBody = amount - contribution;  // тело кредита
+  
+  function alertNan(n, nameVar) {
+    if (isNaN(parseFloat(n)) || isNaN(n - 0)) {
+      return `Параметр "${nameVar}" содержит неправильное значение "${n}"`;
+    } else {
+      return true;
+    }
+  }
 
-  // код для задачи №2 писать здесь. 
+  function monthDiff(dateFrom, dateTo) {
+    return dateTo.getMonth() - dateFrom.getMonth() + (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
+  }
 
-  return totalAmount;
+  let percentLog = alertNan(percent, 'Процентная ставка');
+  let contributionLog = alertNan(contribution, 'Начальный взнос');
+  let amountLog = alertNan(amount, 'Общая стоимость');
+
+  if (percentLog !== true) {
+    totalAmount = percentLog;
+    return totalAmount;
+  } else if (contributionLog !== true) {
+    totalAmount = contributionLog;
+    return totalAmount;
+  } else if (amountLog !== true) {
+    totalAmount = amountLog;
+    return totalAmount;
+  } else {
+    
+    percent = parseFloat(percent);
+    contribution = parseFloat(contribution);
+    amount = parseFloat(amount);
+
+    let numberOfMonths = monthDiff(new Date(), new Date(date));  // количество месяцев
+    let payment = loanBody * (percent / 12 + (percent / 12 / (((1 + percent / 12) ** numberOfMonths) - 1)));  // платеж
+    totalAmount = Math.floor((payment * numberOfMonths + contribution) * 100) / 100 ;  // платеж * кол-во месяцев + Начальный взнос
+
+    console.log(totalAmount);
+    return totalAmount;
+  }
+
+
 }
