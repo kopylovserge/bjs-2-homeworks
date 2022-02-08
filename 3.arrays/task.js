4c0e388b82f8186a7e9f9c09952d1afd60b67191
Kopylov Sergey <kopylov.serge@gmail.com>
2022-02-08 18:49:38 +0300

    ДЗ к лекции 'Массивы'

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,8 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return arr1.length == arr2.length && arr1.every((element,i) => element === arr2[i]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10)
   return resultArr; // array
 }
