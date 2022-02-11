a2ec576d8a536ce22f1ee88d07abe5be2f845e4c
Kopylov Sergey <kopylov.serge@gmail.com>
2022-02-11 11:00:59 +0300

    ДЗ к лекции 'Объекты'

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,49 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) { 
+    this.marks = [mark];
+    } else {
+    this.marks.push(mark)
+    }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  for (let i = 0; i < marks.length; i++) {
+    if (this.marks === undefined) {
+      this.marks = []
+    }
+    this.marks.push(marks[i]);
+    }
+}
+
+Student.prototype.getAverage = function() {
+  if (this.marks) {
+    const average = this.marks.reduce((acc, item, idx, arr) => {
+      if (idx === arr.length - 1) {
+      return (acc + item) / arr.length;
+      } else {
+        return acc + item;
+      }
+      })
+    return average;
+  }
+}
+
+Student.prototype.exclude = function(reason) {
+  if (this.subject) {
+    delete this.subject;
+  }
+  if (this.marks) {
+    delete this.marks;
+  }
+  this.excluded = reason;
+}
